35de630bfd477281cc8364a1575aa0a7630d1cc4
Amjed Omar <amjedomar201@gmail.com>
2021-12-02 00:35:46 +0300

    fix: correct phrase typos

--- src/data/phrases.ts
+++ src/data/phrases.ts
@@ -1,19 +1,19 @@
 const phrases = {
   imageUploadFailed: {
-    ar: 'حدث خطأ ما أثناء رفع الصورة، فضلًا تأكد من إتصالك بالإنترنت',
+    ar: 'حدث خطأ ما اثناء رفع الصورة، فضلًا تأكد من إتصالك بالإنترنت',
     en: 'An error occurred while uploading the image. Please make sure you are connected to the internet',
   },
   enterLink: {
-    ar: 'من فضلك، أدخل الرابط',
+    ar: 'من فضلك، ادخل الرابط',
     en: 'Please enter the link',
   },
   invalidLink: {
-    ar: 'الرابط الذي أدخلته غير صالح',
+    ar: 'الرابط الذي ادخلته غير صالح',
     en: 'The entered link is invalid',
   },
   invalidImage: {
     ar: 'الصورة التي رفعتها غير صالحة',
-    en: 'The entered link is invalid',
+    en: 'The uploaded image is invalid',
   },
 };
 
